7c5ccca5f8e5040b7bf6334f96f668bddd1e9c74
Gene Bederak <gbederak@rockportllc.com>
2024-04-11 06:57:24 -0400

    updates to console sample

--- ConsoleAppViaAppSettings/Program.cs
+++ ConsoleAppViaAppSettings/Program.cs
@@ -7,8 +7,7 @@ var host = Host.CreateDefaultBuilder().Build();
 var config = host.Services.GetService<IConfiguration>();
 var apiKey = config["apikey"];
 
-var environment = host.Services.GetService<IHostEnvironment>();
-Console.WriteLine($"Current Environment: {environment.EnvironmentName}");
+Console.WriteLine($"Current Environment: {host.Services.GetService<IHostEnvironment>().EnvironmentName}");
 
 Console.WriteLine($"API Key: {apiKey}");
 
